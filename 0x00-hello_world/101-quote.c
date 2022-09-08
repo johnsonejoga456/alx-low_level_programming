2b434bb44e821ac304b4397caeb861c89b84087a
johnsonejoga456 <ejogajohnson@gmail.com>
2022-09-08 06:17:06 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,21 @@
-#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
  */
 
-int write(int filedes, const char *buf, unsigned int nbyte);
+#include <unistd.h>
+
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59);
+
 	return (1);
 }

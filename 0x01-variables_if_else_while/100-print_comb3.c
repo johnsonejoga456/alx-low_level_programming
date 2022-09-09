5e1709520d2275025835a06e7afa5192bac252ef
johnsonejoga456 <ejogajohnson@gmail.com>
2022-09-09 08:58:10 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,21 +11,21 @@ int main(void)
 
 	for (i = '0' ; c <= '9' ; c++)
 	{
-	for (i = '0' ; i <= '9' ; i++)
-	{
-	if (c < i)
-	{
-		putchar(c);
-		putchar(i);
+		for (i = '0' ; i <= '9' ; i++)
+		{
+			if (c < i)
+			{
+				putchar(c);
+				putchar(i);
 
-	if (c != '8' || (c == '8' && i != '9'))
-	{
-		putchar(',');
-		putchar(' ');
+				if (c != '8' || (c == '8' && i != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
-	}
-	}
-	}
-		putchar('\n');
-		return (0);
+	putchar('\n');
+	return (0);
 }

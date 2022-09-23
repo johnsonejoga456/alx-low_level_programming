9150d9fb0100815f95bad9e3cdf857f62d0c9132
johnsonejoga456 <ejogajohnson@gmail.com>
2022-09-22 17:07:16 -0700

    Task 9, main.h and Task 10

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,18 +1,18 @@
-#ifndef _main_h_
-#define _main_h_
+#ifndef MAIN_H
+#define MAIN_H
 
-void reverse_array(int *a, int n);
-void print_number(int n);
-void print_buffer(char *b, int size);
-int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+int _putchar(char);
 char *rot13(char *);
+void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-char *add_strings(char *n1, char *n2, char *r, int r_index);
+void print_buffer(char *b, int size);
 
 #endif
